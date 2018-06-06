5e4e93c67e79c900b3df346a7cb8451fabbb1ff3
李贤 <xianguo@maihaoche.com>
2018-06-06 19:46:28 +0800

    bug fix

--- jeep-common/src/main/java/com/leno/jeep/common/net/HttpService.java
+++ jeep-common/src/main/java/com/leno/jeep/common/net/HttpService.java
@@ -4,7 +4,6 @@ package com.leno.jeep.common.net;
 import com.leno.jeep.common.utils.AreaHelper;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 import java.util.List;
 
@@ -16,12 +15,15 @@ import java.util.List;
  */
 public interface HttpService {
 
-    @GET("http://www.500d.me/cvresume/{id}/")
-    Call<String> webCrawler(@Path("id") Long id);
-
-    @GET("https://raw.githubusercontent.com/LenoXian/common-data/master/city.json")
+    /**
+     * 城市信息
+     */
+    @GET("https://raw.githubusercontent.com/DomoXian/common-data/master/city.json")
     Call<List<AreaHelper.CityItem>> getCityInfo();
 
-    @GET("https://raw.githubusercontent.com/LenoXian/common-data/master/province.json")
+    /**
+     * 省份信息
+     */
+    @GET("https://raw.githubusercontent.com/DomoXian/common-data/master/province.json")
     Call<List<AreaHelper.ProvinceItem>> getProvinceInfo();
 }
--- jeep-service/src/main/java/com/leno/jeep/controller/MsgController.java
+++ jeep-service/src/main/java/com/leno/jeep/controller/MsgController.java
@@ -2,7 +2,6 @@ package com.leno.jeep.controller;
 
 import com.leno.jeep.common.Result;
 import com.leno.jeep.service.MsgService;
-import com.leno.jeep.service.WebCrawlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,20 +20,10 @@ public class MsgController {
     @Autowired
     private MsgService msgService;
 
-    @Autowired
-    private WebCrawlService webCrawlService;
-
     @GetMapping("/getMsgList.json")
     public Result getMsgList() {
         return msgService.getCarMsgList();
     }
-
-    @GetMapping("/getCrawlResume.json")
-    public Result getCrawlList() {
-        new Thread(() -> webCrawlService.webCrawlResumeList()).start();
-        return Result.getSuccessResult("成功");
-    }
-
 
 
 }
